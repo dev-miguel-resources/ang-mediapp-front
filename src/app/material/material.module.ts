556fd827fd1f167a5375d57840d8b0be15837340
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-07-05 15:48:59 -0400

    changes in app component html

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
